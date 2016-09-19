f255337d40255555bfa2b509a4ae2028f247f1d8
Nicholas Raphael <Nicholas Raphael>
2016-09-19 18:52:54 -0400

    Auth project using passport

---
+++ nodeauth/routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register',function(req, res, next) {
+  res.render('register', {title:'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title:'Login'});
+});
+
+router.post('/register', upload.single('profileimage'), function (req, res, next) {
+  console.log(req.body.name);
+});
+
+module.exports = router;
